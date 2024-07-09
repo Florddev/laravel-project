598acb2529baaaf3049f4ca55b50b8035caf5704
Florddev <florddev@protonmail.com>
2024-07-09 19:37:55 +0200

    Mise en place de DailyUI et ajout du compteur Livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
+Route::get('/counter', Counter::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
